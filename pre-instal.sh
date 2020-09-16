7d652f16607b29c1b2843be09d0683147089a4aa
Vlad Kamerdinerov <61966975+v-kamerdinerov@users.noreply.github.com>
2020-09-16 12:47:53 +0300

    Update pre-instal.sh
    add postgres

--- pre-instal.sh
+++ pre-instal.sh
@@ -5,3 +5,6 @@ sudo apt upgrade -y
 sudo apt install make g++ apache2 unzip p7zip-full lua5.1 lua5.1-dev lua-socket -y
 sudo a2enmod cgi
 systemctl restart apache2
+
+sudo apt-get install -y libpq-dev
+sudo apt-get install -y postgresql
