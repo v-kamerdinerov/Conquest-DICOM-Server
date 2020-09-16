3e9c32dd43403efba07b4c9bf26cfb114cdd2e2a
Vlad Kamerdinerov <61966975+v-kamerdinerov@users.noreply.github.com>
2020-09-16 12:45:01 +0300

    Update pre-instal.sh

--- pre-instal.sh
+++ pre-instal.sh
@@ -2,6 +2,6 @@
 # Pre-isntall software
 sudo apt update 
 sudo apt upgrade -y
-sudo apt install make g++ apache2 unzip p7zip-full lua5.1 ua5.1-dev lua-socket -y
+sudo apt install make g++ apache2 unzip p7zip-full lua5.1 lua5.1-dev lua-socket -y
 sudo a2enmod cgi
 systemctl restart apache2
